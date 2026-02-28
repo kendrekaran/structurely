4001d68ae60f9f1913e1a84ed1e653eb8f56bdb1
Vipul Kumar <email.coex@gmail.com>
2026-03-01 02:12:24 +0530

    refactor: enhance Separator component layout and styling
    - Simplified the class structure in the Separator component for improved readability.
    - Adjusted the layout to better encapsulate the border styling and ensure consistent spacing.

--- components/_ui/separator.tsx
+++ components/_ui/separator.tsx
@@ -7,13 +7,14 @@ interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
 export default function Separator({ className, ...props }: SeparatorProps) {
   return (
     <div
-      className={cn(
-        "relative flex items-center justify-center border-t border-dashed border-[#E5E7EB]",
-        className,
-      )}
+      className={cn("border-t border-dashed border-[#E5E7EB]", className)}
       {...props}
     >
-      <div className="max-w-global absolute bottom-0 mx-auto w-full border-t-1 border-[#E5E7EB]" />
+      <div className="px-global">
+        <div className="max-w-global relative mx-auto">
+          <div className="absolute bottom-0 w-full border-t border-[#E5E7EB]" />
+        </div>
+      </div>
     </div>
   );
 }
