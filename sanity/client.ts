1a7c79e36a74d2ddc06af0670b0a6afd746a0ccd
Vipul Kumar <email.coex@gmail.com>
2026-03-11 05:22:44 +0530

    chore: update package dependencies and remove unused imports
    - Removed unused dependencies from package.json and package-lock.json, specifically @sanity/image-url and next-sanity.
    - Updated various Babel packages to their latest versions for improved performance and security.
    - Commented out unused code in sanity/client.ts to clean up the file and prevent unnecessary imports.

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,16 +1,16 @@
-import {
-  createImageUrlBuilder,
-  type SanityImageSource,
-} from "@sanity/image-url";
-import { createClient } from "next-sanity";
+// import {
+//   createImageUrlBuilder,
+//   type SanityImageSource,
+// } from "@sanity/image-url";
+// import { createClient } from "next-sanity";
 
-export const client = createClient({
-  projectId: "d3i8jodu",
-  dataset: "production",
-  apiVersion: "2024-01-01",
-  useCdn: false,
-});
+// export const client = createClient({
+//   projectId: "d3i8jodu",
+//   dataset: "production",
+//   apiVersion: "2024-01-01",
+//   useCdn: false,
+// });
 
-const builder = createImageUrlBuilder(client);
+// const builder = createImageUrlBuilder(client);
 
-export const urlFor = (source: SanityImageSource) => builder.image(source);
+// export const urlFor = (source: SanityImageSource) => builder.image(source);
