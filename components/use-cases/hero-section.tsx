061b9e55e72425dc800fd3cf2a02c16633e8a44c
Vipul Kumar <email.coex@gmail.com>
2026-04-13 01:41:37 +0530

    style: refine hero section layout and typography
    - Adjusted the layout of the HeroSection component for improved responsiveness and visual appeal.
    - Enhanced typography by modifying class names for better consistency across different screen sizes.
    - Maintained overall structure while ensuring a cohesive design in the hero section.

--- components/use-cases/hero-section.tsx
+++ components/use-cases/hero-section.tsx
@@ -2,14 +2,18 @@ function HeroSection() {
   return (
     <section id="hero" className="relative z-0">
       <div className="px-global">
-        <div className="max-w-global py-[64px] md:py-section-md mx-auto flex flex-col items-center gap-3.5 md:gap-4 px-[12px] md:px-0 border-x border-[#E5E7EB]">
-          <h2 data-reveal="words" className="max-w-[11.08em] md:max-w-[653px] text-center text-[32px] md:text-[48px] leading-[44px] md:leading-[54px] font-normal md:font-medium tracking-[-0.04em] md:tracking-[-0.03em]">
-          This is built for high-volume revenue teams that need to scale and nurture.
+        <div className="max-w-global md:py-section-md mx-auto flex flex-col items-center gap-3.5 border-x border-[#E5E7EB] px-[12px] py-[64px] md:gap-4 md:px-0">
+          <h2
+            data-reveal="words"
+            className="max-w-[11.08em] text-center text-[32px] leading-[44px] font-medium tracking-[-0.04em] md:max-w-[653px] md:text-[48px] md:leading-[54px] md:tracking-[-0.03em]"
+          >
+            This is built for high-volume revenue teams that need to scale and
+            nurture.
           </h2>
-          <p className="max-w-[35em] md:max-w-[742px] text-center text-[16px] md:text-[18px] leading-[26px] md:leading-[28px] tracking-[-0.01em]">
+          <p className="max-w-[35em] text-center text-[16px] leading-[26px] tracking-[-0.01em] md:max-w-[742px] md:text-[18px] md:leading-[28px]">
             <span className="text-[#006FFF]">Structurely AI</span> supports any
-            organization where speed, qualification,<br/> and follow-up determine
-            revenue.
+            organization where speed, qualification,
+            <br /> and follow-up determine revenue.
           </p>
         </div>
       </div>
